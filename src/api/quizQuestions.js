7c03bb5d7b70b31bd0d6bcf2cf089b5a9d08ab04
Richard Patsch <richard.patsch@stud.fh-campuswien.ac.at>
2018-11-24 15:13:51 +0100

    Update questions

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,88 +1,19 @@
-var quizQuestions = [
+const quizQuestions = [
   {
-      question: "What franchise would you rather play a game from?",
+      id: 0,
+      question: "first Question?",
       answers: [
-          {
-              type: "Microsoft",
-              content: "Halo"
-          },
-          {
-              type: "Nintendo",
-              content: "Pokemon"
-          },
-          {
-              type: "Sony",
-              content: "Uncharted"
-          }
-      ]
+        "first", "second", "third", "fourth"
+      ],
+      correctAnswers: [0,2,3]
   },
   {
-      question: "Which console would you prefer to play with friends?",
+      id: 1,
+      question: "Second question?",
       answers: [
-          {
-              type: "Microsoft",
-              content: "X-Box"
-          },
-          {
-              type: "Nintendo",
-              content: "Nintendo 64"
-          },
-          {
-              type: "Sony",
-              content: "Playstation 1"
-          }
-      ]
-  },
-  {
-      question: "Which of these racing franchises would you prefer to play a game from?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "Forza"
-          },
-          {
-              type: "Nintendo",
-              content: "Mario Kart"
-          },
-          {
-              type: "Sony",
-              content: "Gran Turismo"
-          }
-      ]
-  },
-  {
-      question: "Which of these games do you think is best?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "BioShock"
-          },
-          {
-              type: "Nintendo",
-              content: "The Legend of Zelda: Ocarina of Time"
-          },
-          {
-              type: "Sony",
-              content: "Final Fantasy VII"
-          }
-      ]
-  },
-  {
-      question: "What console would you prefer to own?",
-      answers: [
-          {
-              type: "Microsoft",
-              content: "X-Box One"
-          },
-          {
-              type: "Nintendo",
-              content: "Wii U"
-          },
-          {
-              type: "Sony",
-              content: "Playstation 4"
-          }
-      ]
+        "first", "second", "third", "fourth"
+      ],
+      correctAnswers: [2,3]
   }
 ];
 
